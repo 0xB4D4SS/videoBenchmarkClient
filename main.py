bdb3981071d1a4e9670012ab466569d4d66d1722
AstereY <pavel.chmykhov@nutnet.ru>
2022-04-03 19:49:46 +0400

    switch to pyqtgraph instead of pyqtcharts, add fps and bitrate graphs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import re
+import pyqtgraph as pg
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QTextEdit, QWidget, QLineEdit
 
@@ -9,33 +11,53 @@ class MainWindow(QWidget):
         super().__init__(parent)
 
         self.address = None
+        self.curr_data = None
+        self.fpsdata = ''
+        self.brdata = ''
+        self.x = list()
+        self.yfps = list()
+        self.ybr = list()
+        self.counter = 0
         self.setWindowTitle("VideoBenchmark Client")
-        self.resize(400, 300)
+        self.resize(800, 600)
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.getsomedata)
+        self.datatimer = QTimer(self)
+        self.drawtimer = QTimer(self)
         self.textEdit = QTextEdit()
         self.socket_data = QLineEdit()
-        self.socket_data.setText("127.0.0.1:5000")
-        self.textEdit.setReadOnly(True)
+        self.pgfpsplot = pg.PlotWidget()
+        self.pgbrplot = pg.PlotWidget()
         self.btn1 = QPushButton("Connect")
         self.btn2 = QPushButton("Stop")
+        self.socket_data.setText("127.0.0.1:5000")
+        self.textEdit.setReadOnly(True)
+        self.btn2.setDisabled(True)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.textEdit)
-        layout.addWidget(self.btn1)
-        layout.addWidget(self.btn2)
-        layout.addWidget(self.socket_data)
-        self.setLayout(layout)
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.textEdit)
+        vlayout.addWidget(self.btn1)
+        vlayout.addWidget(self.btn2)
+        vlayout.addWidget(self.socket_data)
+        vlayout.addWidget(self.pgfpsplot)
+        vlayout.addWidget(self.pgbrplot)
+        self.setLayout(vlayout)
 
         self.btn1.clicked.connect(self.btn1_clicked)
         self.btn2.clicked.connect(self.btn2_clicked)
+        self.datatimer.timeout.connect(self.getsomedata)
+        self.drawtimer.timeout.connect(self.drawsomedata)
 
     def btn1_clicked(self):
-        self.timer.start(1000)
+        self.datatimer.start(1000)
+        self.drawtimer.start(1001)
+        self.btn1.setDisabled(True)
+        self.btn2.setDisabled(False)
 
     def btn2_clicked(self):
-        self.timer.stop()
+        self.datatimer.stop()
+        self.drawtimer.stop()
+        self.btn1.setDisabled(False)
+        self.btn2.setDisabled(True)
 
     def getsomedata(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,7 +67,27 @@ class MainWindow(QWidget):
         sock.connect(self.address)
         sock.sendall(bytes("getdata\n", "utf-8"))
         data = str(sock.recv(1024), "utf-8")
-        self.textEdit.append(data)
+        self.curr_data = data
+        fpsregexp = re.compile("fps= ([0-9]*)")
+        brregexp = re.compile("bitrate= ([0-9.]*)")
+        self.fpsdata = fpsregexp.search(data).group(1)
+        self.brdata = brregexp.search(data).group(1)
+        self.textEdit.append(self.curr_data)
+
+    def drawsomedata(self):
+        self.counter += 1
+        self.x.append(self.counter)
+        self.yfps.append(int(self.fpsdata))
+        self.ybr.append(float(self.brdata))
+        self.pgfpsplot.plot(self.x, self.yfps, pen='g')
+        self.pgbrplot.plot(self.x, self.ybr, pen='g')
+        if self.counter > 60:
+            self.counter = 0
+            self.x.clear()
+            self.yfps.clear()
+            self.ybr.clear()
+            self.pgfpsplot.clear()
+            self.pgbrplot.clear()
 
 
 if __name__ == '__main__':
